be4a0fd8a8954c359c6834aeec286f9197f93683
rgerk <r.gerk@hotmail.com>
2020-02-28 09:23:23 -0300

    Add files via upload

---
+++ variavel_numero.py
@@ -0,0 +1,7 @@
+nota1 = int( input("Digite a nota 1: "))
+nota2 = int( input("Digite a nota 2: "))
+
+media = (nota1 + nota2) / 2
+
+print("Sua média é:\t", media)
+print("Sua média é:\t" + str(media))
